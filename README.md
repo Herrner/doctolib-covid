40c2fce6664347d7a1e76b702fbd70eb4cd51c8a
kai <kai.woerner@uni-hamburg.de>
2021-05-31 13:48:47 +0200

    first commit

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 This script sends email alerts (using Google SMTP server) when vaccination appointments are available on Doctolib.
 
-My modification tries to change and expand the script to handle german doctolib-servers (done), look for all sorts of vaccination appointments (not only BioNTech, done), generate a link to the appointment page (doing) and (maybe) search for centers that offer appointments for first-time visitors (todo).
-
-*The following is unchanged from the original repository:*
+My modification lets you select the desired vaccine (from the list in vaccines.txt) via a prompt, works with german 
+Doctolib-Servers and constantly retries to find appointments.  
+The list of centers still has to be created by hand - the example shows centers that accept first-time visitors for 
+vaccination in Hamburg.
 
 ## Prerequisites
  
@@ -28,30 +29,20 @@ My modification tries to change and expand the script to handle german doctolib-
 `$ python doctolib-covid.py`
 
 - Output example
-```
-0 appointments available at Centre de Vaccination - Salle Olympe de Gouges - 15 Rue Merlin, 75011 Paris
-0 appointments available at Centre de Vaccination - Centre Bertheau - 15-17 Rue Charles Bertheau, 75013 Paris
-0 appointments available at Centre de Vaccination - Mairie Paris Centre - 2 Rue Eugène Spuller, 75003 Paris
-5 appointments available at Centre de Vaccination - Mairie du 13e - 1 Place d'Italie, 75013 Paris
-  --> Alert sent to me@bntan.com
-0 appointments available at Centre de Vaccination - Le 104 - 5 Rue Curial, 75019 Paris
-0 appointments available at Centre de Vaccination - Paris 20e - 87 Rue des Haies, 75020 Paris
-0 appointments available at Centre de vaccination Aubrac - 10 Rue de l'Aubrac, 75012 Paris
-0 appointments available at Centre de Vaccination - Mairie du 10e - 72 Rue du Faubourg Saint-Martin, 75010 Paris
-[...]
-```
+```bash
+0. biontech
+1. janssen
+2. astrazeneca
+3. moderna
+4. scheissemiterdbeeren
+5. alle Impfstoffe
+Bitte wählen: (0, 1, 2, 3, 4, 5): 0
+0 Termin(e) verfügbar bei Dr. med. Tom Straessle und Frau Dr. Ulrike Niehaus Fachärzte für Allgemeinmedizin, Großneumarkt 20 Hamburg-Mitte, 20459 Hamburg
+Hausarztpraxis St. Pauli: bietet keine entsprechenden Impfungen an
+0 Termin(e) verfügbar bei Schmerztherapie am Rothenbaum, Heimhuder Straße 38, 20148 Hamburg-Rotherbaum
+0 Termin(e) verfügbar bei Anästhesiepraxis Walsrode / Augenpartner Walsrode , Lange Straße 55, 29664 Walsrode
+Herr Dr. med. Peter Kühnelt: bietet keine entsprechenden Impfungen an
+Hausarzt Forum Winterhude: bietet keine entsprechenden Impfungen an
+Nächster Versuch in 5 Minuten
 
-## Cron
-
-Use [cron-doctolib-covid.yml](../master/.github/workflows/cron-doctolib-covid.yml) to schedule the script execution. Uncomment the `schedule` section.
-The example below executes the script every 5 minutes
-```yaml
-name: Cron Doctolib COVID
-
-on:
-  [...]
-  schedule:
-    - cron: "*/5 * * * *"
-
-[...]
 ```
--- doctolib-covid.py
+++ doctolib-covid.py
@@ -1,74 +1,126 @@
 import os
 import datetime
+import time
+
+import click
 import requests
 import smtplib, ssl
 
-DISABLE_EMAIL = os.environ.get("DISABLE_EMAIL")
+DISABLE_EMAIL = os.environ.get("ENABLE_EMAIL", True)
 SENDER_EMAIL = os.environ.get("SENDER_EMAIL")
 SENDER_PASSWORD = os.environ.get("SENDER_PASSWORD")
 RECEIVER_EMAIL = os.environ.get("RECEIVER_EMAIL")
+RETRY_SPAN = 300
 
 with open('centers.txt') as centers_txt:
     centers = centers_txt.readlines()
 centers = [center.strip() for center in centers
-           if not center.startswith("#")] 
+           if not center.startswith("#")]
 
-for center in centers:
-    
-    data = requests.get(f"https://www.doctolib.fr/booking/{center}.json").json()["data"]
-    
-    visit_motives = [visit_motive for visit_motive in data["visit_motives"]
-                     if visit_motive["name"].startswith("1re injection") and 
-                     "AstraZeneca" not in visit_motive["name"]]
-    if not visit_motives:
-        continue
-    
-    places = [place for place in data["places"]]
-    if not places:
-        continue
-    
-    for place in places:
-        
-        start_date = datetime.datetime.today().date().isoformat()
-        visit_motive_ids = visit_motives[0]["id"]
-        practice_ids = place["practice_ids"][0]
-        place_name = place["formal_name"]
-        place_address = place["full_address"]
-        
-        agendas = [agenda for agenda in data["agendas"]
-                   if agenda["practice_id"] == practice_ids and
-                   not agenda["booking_disabled"] and
-                   visit_motive_ids in agenda["visit_motive_ids"]]
-        if not agendas:
+
+def checkVaccine(motive, vaccines):
+    vac_check = False
+    for vaccine in vaccines:
+        if vaccine in motive.lower():
+            vac_check = True
+    return vac_check
+
+
+def checkAppointments(vaccines):
+    for center in centers:
+        response = requests.get(f"https://www.doctolib.de/booking/{center}.json")
+        data = response.json()["data"]
+        bookingurl =  (f"https://www.doctolib.de/booking/{center}")
+        visit_motives = [visit_motive for visit_motive in data["visit_motives"]
+                         if (visit_motive["name"].startswith("Erstimpfung")
+                         or visit_motive["name"].startswith("Einzelimpfung"))
+                         and checkVaccine(visit_motive["name"], vaccines)]
+        if not visit_motives:
+            click.echo(click.style(f"{data['profile']['name_with_title']}: bietet keine entsprechenden Impfungen an", fg='red'))
             continue
-        
-        agenda_ids = "-".join([str(agenda["id"]) for agenda in agendas])
-               
-        # print(visit_motive_ids)
-        # print(practice_ids)
-        # print(agenda_ids)
-        
-        response = requests.get(
-                "https://www.doctolib.fr/availabilities.json",
-                params = {
+
+        places = [place for place in data["places"]]
+        if not places:
+            continue
+        for place in places:
+#            for motive in visit_motives:
+                start_date = datetime.datetime.today().date().isoformat()
+                visit_motive_ids = [motive["id"] for motive in visit_motives]
+                practice_ids = place["practice_ids"][0]
+                place_name = place["formal_name"] if len(place["formal_name"])>0 else data['profile']['name_with_title']
+                place_address = place["full_address"]
+
+                agendas = [agenda for agenda in data["agendas"]
+                           if agenda["practice_id"] == practice_ids and
+                           not agenda["booking_disabled"] and
+                           any(x in visit_motive_ids for x in agenda["visit_motive_ids"])]
+#                           any(x in a for x in b)
+#                           visit_motive_ids in agenda["visit_motive_ids"]]
+                if not agendas:
+                    print("not agenda")
+                    continue
+
+                agenda_ids = "-".join([str(agenda["id"]) for agenda in agendas])
+
+                # print(visit_motive_ids)
+                # print(practice_ids)
+                # print(agenda_ids)
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+                response = requests.get(
+                    "https://www.doctolib.de/availabilities.json",
+                    params={
                         "start_date": start_date,
                         "visit_motive_ids": visit_motive_ids,
                         "agenda_ids": agenda_ids,
                         "practice_ids": practice_ids,
                         "insurance_sector": "public",
                         "destroy_temporary": "true",
-                        "limit":2
-                },
-        )
-        response.raise_for_status()
-        nb_availabilities = response.json()["total"]
-        
-        result = str(nb_availabilities) + " appointments available at " + place_name + " - " + place_address
-        print(result)
-        
-        if nb_availabilities > 0 and DISABLE_EMAIL != "true":
-            context = ssl.create_default_context()
-            with smtplib.SMTP_SSL("smtp.gmail.com", 465, context = context) as server:
-                server.login(SENDER_EMAIL, SENDER_PASSWORD)
-                server.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, result.encode('utf-8'))
-                print("  --> Alert sent to " + RECEIVER_EMAIL)
+                        "limit": 2
+                    }, headers=headers
+                )
+                response.raise_for_status()
+#                print(response.json())
+                nb_availabilities = response.json()["total"]
+# show availabilities
+                result = f"{str(nb_availabilities)} Termin(e) verfügbar bei {place_name}, {place_address}{os.linesep+bookingurl if nb_availabilities > 0 else ''}"
+                if (nb_availabilities>0):
+                    click.echo(click.style(result, fg='green'))
+                else:
+                    click.echo(result)
+    if nb_availabilities > 0 and DISABLE_EMAIL != "true":
+                    context = ssl.create_default_context()
+                    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+                        server.login(SENDER_EMAIL, SENDER_PASSWORD)
+                        server.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, result.encode('utf-8'))
+                        print("  --> Alert sent to " + RECEIVER_EMAIL)
+
+def selectVaccines():
+    with open('vaccines.txt') as vaccines_txt:
+        vaccines = vaccines_txt.readlines()
+    vaccines = [vaccine.strip().lower() for vaccine in vaccines]
+    for i, vaccine in enumerate(vaccines):
+        click.echo(f"{i}. {vaccine}")
+    click.echo(f"{len(vaccines)}. alle Impfstoffe"),
+    choice = click.prompt(
+        "Bitte wählen:",
+        type=click.Choice([str(i) for i in range(len(vaccines) + 1)]),
+        show_default=False,
+        default='0'
+    )
+    choice = int(choice)
+    useVacs = []
+    if choice == len(vaccines):
+        useVacs = vaccines
+    else:
+        useVacs.append(vaccines[choice])
+    return useVacs
+
+# Go
+vaccines = selectVaccines()
+while True:
+    checkAppointments(vaccines)
+    print("Nächster Versuch in 5 Minuten")
+    if RETRY_SPAN==0:
+        continue
+    time.sleep(RETRY_SPAN)
